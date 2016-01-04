92f58e444e8ff1142b9580231adf5a86a15ef62f
clearbug <craftsman_gao@qq.com>
2016-01-04 11:07:33 +0800

    Implement A Current Time Middleware

--- bin/mini-harp.js
+++ bin/mini-harp.js
@@ -1,8 +1,18 @@
 #!/usr/bin/env node
 var connect = require('connect');
 var parseArgs = require('minimist');
+var url = require('url');
 var args = parseArgs(process.argv.slice(2));
 var app = connect();
 var port = args['port']?args['port']:4000;
 console.log('Starting mini-harp on http://localhost:' + port);
-app.listen(port);
+app
+	.use(function(request, response){
+		var path = url.parse(request.url).path;
+		if(path === '/current-time'){ 
+			var date = (new Date()).toISOString();
+			response.end(date + '\n');
+		}else{
+			response.end('Cannot Get ' + path + '\n');
+		}
+	}).listen(port);
--- example.js
+++ example.js
@@ -1,3 +1,17 @@
-var parseArgs = require('minimist');
-var args = parseArgs(process.argv.slice(2));
-console.dir(args);
+var connect = require('connect');
+connect()
+	.use(connect.cookieParser())
+	.use(connect.session({secret: 'some secret text', cookie: {maxAge: 30000}}))
+	.use(function(req, res){
+		var sess = req.session,
+			  url = req.url.split('/');
+		if(url[1] == 'name' && url[2]){
+			sess.name = url[2];
+			res.end('name saved: ' + url[2]);
+		}else if(sess.name){
+			res.write('session-stored name: ' + sess.name);
+			res.end('stored for another: ' + (sess.cookie.maxAge / 1000) + ' seconds');
+		}else{
+			res.end('no stored name; go to /name/{name} to save a name');
+		}
+}).listen(3000);
