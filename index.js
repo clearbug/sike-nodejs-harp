1b0540d222582b880dc8b515eea46d0c53a24165
clearbug <craftsman_gao@qq.com>
2016-01-04 18:37:00 +0800

    Rendering Jade Templates

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 var connect = require('connect');
 var serveStatic = require('serve-static');
+var makeJade = require('./lib/processor/jade');
 function miniHarp(root){
 	var app = connect();
-	app.use(serveStatic(root));
+	app
+		.use(serveStatic(root))
+		.use(makeJade(root));
+		
 	return app;
 };
 module.exports = miniHarp;
