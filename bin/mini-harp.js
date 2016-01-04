605e6560792ce9f1e176e0c6fb0039fb32e73b70
clearbug <craftsman_gao@qq.com>
2016-01-04 11:41:54 +0800

    Serving Static Content

--- bin/mini-harp.js
+++ bin/mini-harp.js
@@ -2,17 +2,12 @@
 var connect = require('connect');
 var parseArgs = require('minimist');
 var url = require('url');
+var serveStatic = require('serve-static');
+
 var args = parseArgs(process.argv.slice(2));
 var app = connect();
 var port = args['port']?args['port']:4000;
+var root = args['_'][0]?args['_'][0]:process.cwd();
 console.log('Starting mini-harp on http://localhost:' + port);
 app
-	.use(function(request, response){
-		var path = url.parse(request.url).path;
-		if(path === '/current-time'){ 
-			var date = (new Date()).toISOString();
-			response.end(date + '\n');
-		}else{
-			response.end('Cannot Get ' + path + '\n');
-		}
-	}).listen(port);
+	.use(serveStatic(root)).listen(port);
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var connect = require('connect');
-function miniHarp(){
+var serveStatic = require('serve-static');
+function miniHarp(root){
 	var app = connect();
+	app.use(serveStatic(root));
 	return app;
 };
 module.exports = miniHarp;
