85e3c174323c4ad41a6d98b28779891862457207
clearbug <craftsman_gao@qq.com>
2016-01-05 10:03:54 +0800

    Rendering Less Templates

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 var connect = require('connect');
 var serveStatic = require('serve-static');
 var makeJade = require('./lib/processor/jade');
+var makeLess = require('./lib/processor/less');
 function miniHarp(root){
 	var app = connect();
 	app
 		.use(serveStatic(root))
-		.use(makeJade(root));
+		.use(makeJade(root))
+		.use(makeLess(root));
 		
 	return app;
 };
