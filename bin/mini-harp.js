433eef4802a4c6cee2be395f5f5022e017800dbd
clearbug <craftsman_gao@qq.com>
2016-01-05 11:57:21 +0800

    Rendering Jade Templates 2

--- bin/mini-harp.js
+++ bin/mini-harp.js
@@ -1,13 +1,10 @@
 #!/usr/bin/env node
-var connect = require('connect');
 var parseArgs = require('minimist');
-var url = require('url');
-var serveStatic = require('serve-static');
+var miniHarp = require('../');
 
 var args = parseArgs(process.argv.slice(2));
-var app = connect();
 var port = args['port']?args['port']:4000;
 var root = args['_'][0]?args['_'][0]:process.cwd();
+var app = miniHarp(root);
 console.log('Starting mini-harp on http://localhost:' + port);
-app
-	.use(serveStatic(root)).listen(port);
+app.listen(port);
